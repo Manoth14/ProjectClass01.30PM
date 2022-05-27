3a6af7892e2585a48fbd8f5fc891ede52f51f3c2
Manoth14 <manothkumar.p@gmail.com>
2022-05-27 14:15:45 +0530

    Added methods in Merin

--- src/test/java/com/sampleclass/Class1.java
+++ src/test/java/com/sampleclass/Class1.java
@@ -16,9 +16,24 @@ public class Class1 {
 		System.out.println("3");
 
 	}
-	
+
 	private void method4() {
 		System.out.println("4");
+
+	}
+
+	private void vijay() {
+		System.out.println("vijay");
+
+	}
+
+	private void ajith() {
+		System.out.println("ajith");
+
+	}
+
+	private void surya() {
+		System.out.println("surya");
 
 	}
 
